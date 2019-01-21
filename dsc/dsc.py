f7df4e10ed31b1b45d713e834e4bdd07649d4764
Muhazerin <muhd.muhazerin@gmail.com>
2019-01-21 13:45:51 +0800

    Update dsc.py
    Changed the algorithm that makes the cipher and store it in json file. This algorithm now uses the json package capabililty to easily load and dump json content to file

--- dsc/dsc.py
+++ dsc/dsc.py
@@ -5,34 +5,36 @@ try:
         keys = json.load(secretKeyFile)
 
 except OSError:
-    with open("secretKey.json", 'w') as secretKeyFile:
-        #plaintext key
-        i = 33
-        plaintextKey = []
-        while i < 127:
-            if i == 34:
-                plaintextKey.append("\\\"")
-            elif i == 92:
-                plaintextKey.append("\\\\")
-            else:
-                plaintextKey.append(chr(i))
-            i += 1
-        plaintextKey.append(" ")
+    secretKey = {}
+    i = 32
+    plaintextKey = []
+    cipherKey = []
+    while i < 127:
+        if i == 34:
+            plaintextKey.append('\\\"')
+            cipherKey.append('\\\"')
+        elif i == 92:
+            plaintextKey.append('\\\\')
+            cipherKey.append('\\\\')
+        else:
+            plaintextKey.append(chr(i))
+            cipherKey.append(chr(i))
+        i += 1
 
-        plaintextKeyStr = '"' + '", "'.join(plaintextKey) + '"'
-        #cipher key
-        random.shuffle(plaintextKey)    # create a cipher key by shuffling the plaintextKey
-        cipherKeyStr = '"' + '", "'.join(plaintextKey) + '"'
-
-        plaintextKey.reverse()     #create a 2nd cipher key by reversing the first cipher key
-        cipherKey2Str = '"' + '", "'.join(plaintextKey) + '"'
-
-        key = "{\n\t\"plaintextKey\": [" + plaintextKeyStr+ "],\n\t\"cipherKey\": [" + cipherKeyStr + "],\n\t\"cipherKey2\": [" + cipherKey2Str + "]\n}"
-        secretKeyFile.write(key)
-
-    with open("secretKey.json", "r") as secretKeyFile:
+    secretKey['plaintextKey'] = plaintextKey
+    random.shuffle(cipherKey)
+    secretKey['cipherKey'] = cipherKey
+    cipherKey2 = []
+    for i in cipherKey:
+        cipherKey2.append(i)
+    cipherKey2.reverse()
+    secretKey['cipherKey2'] = cipherKey2
+    
+    with open('secretKey.json','w') as secretKeyFile:
+        json.dump(secretKey, secretKeyFile)
+        
+    with open('secretKey.json','r') as secretKeyFile:
         keys = json.load(secretKeyFile)     #initialize keys to start using the cipher
-
 def encode(data):
     print("encoding\n")
 
